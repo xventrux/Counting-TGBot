a2caaa2de3d75e1dd12d5ab7d50c1f86cb2de84e
Vyacheslav <vvyacheskavx@gmail.com>
2021-09-24 22:16:29 +0500

    Создал фабрику миграций

---
+++ CountingBot.Migrations/MigrationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CountingBot.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingBot.Migrations
+{
+    public class MigrationDbContext : BaseDbContext
+    {
+        public MigrationDbContext(DbContextOptions<MigrationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
