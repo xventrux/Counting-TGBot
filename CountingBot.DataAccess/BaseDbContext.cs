f3def9fa4b8dad6dc13fb54aa2fec8b27064e66b
Vyacheslav <vvyacheskavx@gmail.com>
2021-09-24 22:07:15 +0500

    Создал конфигурацию пользователя

---
+++ CountingBot.DataAccess/BaseDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CountingBot.DataAccess.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingBot.DataAccess
+{
+    public class BaseDbContext : DbContext
+    {
+        public BaseDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
