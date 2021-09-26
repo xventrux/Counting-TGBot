e65a4d51b721a22d9b89b4fb138611a69de08a27
Vyacheslav <vvyacheskavx@gmail.com>
2021-09-26 16:43:44 +0500

    После того, как мы добавили строку подключения в appsettings.json в проекте migrations и подключили пакеты entityframeworkcore.design и entityframeworkcore.tools, создаем первую миграцию

---
+++ CountingBot.Migrations/Migrations/20210926114004_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CountingBot.Migrations.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    Exp = table.Column<int>(type: "int", nullable: false),
+                    ProcessName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Step = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
