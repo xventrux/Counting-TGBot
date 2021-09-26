074b067546750070b7075746a83484ea6e721e81
Vyacheslav <vvyacheskavx@gmail.com>
2021-09-26 17:12:01 +0500

    Добавил контроллер для работы с ботом

---
+++ CountingBot/Controllers/BotController.cs
@@ -0,0 +1,54 @@
+﻿using CountingBot.AppServices.Services.CommandService;
+using CountingBot.Telegram;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace CountingBot.Controllers
+{
+    /// <summary>
+    /// Контроллер для работы с ботом
+    /// </summary>
+    [ApiController]
+    [Route("api/message/update")]
+    public class BotController : ControllerBase
+    {
+        private readonly ITelegramBotClient telegramBotClient;
+        private readonly ICommandService commandService;
+
+        public BotController(ITelegramBotClient telegramBotClient, ICommandService commandService)
+        {
+            this.telegramBotClient = telegramBotClient;
+            this.commandService = commandService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Started");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Update update)
+        {
+
+            await telegramBotClient.SetMyCommandsAsync(new List<BotCommand>()
+            {
+                new BotCommand() { Command = "/start", Description = "Показать список команд" }
+            });
+            //Если обновление пустое, то ничего не делаем
+            if (update == null) return Ok();
+
+            TelegramMessage message = updateService.Check(update);
+
+            if (message == null) return Ok();
+
+            return Ok();
+        }
+    }
+}
