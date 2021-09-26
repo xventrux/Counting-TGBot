5c08eaeb6c6f068a67edf60aca197ed7cfb55ed7
Vyacheslav <vvyacheskavx@gmail.com>
2021-09-26 16:53:37 +0500

    Описал интерфейс сервиса для работы с коммандами

---
+++ CountingBot.AppServices/Services/CommandService/ICommandService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingBot.AppServices.Services.CommandService
+{
+    /// <summary>
+    /// Сервис для работы с коммандами
+    /// </summary>
+    public interface ICommandService
+    {
+        /// <summary>
+        /// Возвращает список команд
+        /// </summary>
+        /// <returns></returns>
+        List<TelegramCommand> GetCommands();
+    }
+}
