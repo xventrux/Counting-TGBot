ab1cd5d14bb3c24fba3a4430d28b0296562c8a47
Vyacheslav <vvyacheskavx@gmail.com>
2021-09-26 16:58:18 +0500

    Описал базовый абстрактный класс телеграм команды

---
+++ CountingBot.Contracts/Base/TelegramCommand.cs
@@ -0,0 +1,39 @@
+﻿using CountingBot.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace CountingBot.Contracts.Base
+{
+    /// <summary>
+    /// Телеграм команда
+    /// </summary>
+    public abstract class TelegramCommand
+    {
+        /// <summary>
+        /// Наименование команды
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// Абстрактный метод, который выполняет команду
+        /// </summary>
+        /// <param name="user">Пользователь</param>
+        /// <param name="msg">Сообщение</param>
+        /// <param name="client">Телеграм клиент</param>
+        /// <returns></returns>
+        public abstract Task<User> Execute(User user, TelegramMessage msg, ITelegramBotClient client);
+        /// <summary>
+        /// Метод, проверяющий текст из параметра на совпадение с именем команды
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public bool Contains(string msg)
+        {
+            return msg.ToLower().Contains(Name.ToLower());
+        }
+    }
+}
