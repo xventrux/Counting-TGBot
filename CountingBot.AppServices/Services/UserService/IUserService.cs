9a894e9f5c9b1a6296ee278fb2eecdd8cb9e83f6
Vyacheslav <vvyacheskavx@gmail.com>
2021-09-26 17:28:43 +0500

    Создал сервис для работы с пользователем

---
+++ CountingBot.AppServices/Services/UserService/IUserService.cs
@@ -0,0 +1,44 @@
+﻿using CountingBot.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingBot.AppServices.Services.UserService
+{
+    /// <summary>
+    /// Сервис для работы с пользователем
+    /// </summary>
+    public interface IUserService
+    {
+        /// <summary>
+        /// Возвращает пользователя из базы данных, либо регистрирует его
+        /// </summary>
+        /// <param name="id">Идентификатор пользователя</param>
+        /// <returns></returns>
+        public Task<User> Login(long id, string login);
+
+        /// <summary>
+        /// Регистрирует пользователя
+        /// </summary>
+        /// <param name="id">Идентификатор пользователя</param>
+        /// <param name="login">Username пользователя</param>
+        /// <returns></returns>
+        public Task<User> Registration(long id, string login);
+
+        /// <summary>
+        /// Обновляет пользователя
+        /// </summary>
+        /// <param name="user">Пользователь</param>
+        /// <returns></returns>
+        public Task Update(User user);
+
+        /// <summary>
+        /// Удаляет пользователя
+        /// </summary>
+        /// <param name="user">Пользователь</param>
+        /// <returns></returns>
+        public Task Delete(User user);
+    }
+}
---
+++ CountingBot.AppServices/Services/UserService/UserService.cs
@@ -0,0 +1,60 @@
+﻿using CountingBot.Domain.Entities;
+using CountingBot.Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingBot.AppServices.Services.UserService
+{
+    /// <inheritdoc/>
+    public class UserService : IUserService
+    {
+        IRepository<User> userRepository;
+
+        public UserService(IRepository<User> userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        /// <inheritdoc/>
+        public async Task<User> Login(long id, string login)
+        {
+            User user = await userRepository.GetByIdAsync(id);
+
+            if (user == null) user = await Registration(id, login);
+
+            return user;
+        }
+
+        /// <inheritdoc/>
+        public async Task<User> Registration(long id, string login)
+        {
+            User user = new User()
+            {
+                Id = id,
+                UserName = login,
+                Exp = 0,
+                Level = 0,
+                ProcessName = "",
+                Step = 0
+
+            };
+            await userRepository.AddAsync(user);
+            return user;
+        }
+
+        /// <inheritdoc/>
+        public async Task Update(User user)
+        {
+            await userRepository.UpdateAsync(user);
+        }
+
+        /// <inheritdoc/>
+        public async Task Delete(User user)
+        {
+            await userRepository.DeleteAsync(user);
+        }
+    }
+}
