aafc8bb65555d9b50731cd9bd2d9c7ad33c5ba4b
Vyacheslav <vvyacheskavx@gmail.com>
2021-09-28 16:20:52 +0500

    Описал класс арифметической задачи

---
+++ CountingBot.Contracts/Base/Problem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingBot.Contracts.Base
+{
+    /// <summary>
+    /// Арифметическая задача
+    /// </summary>
+    public abstract class BaseProblem
+    {
+        /// <summary>
+        /// Ответ
+        /// </summary>
+        public int Answer { get; set; }
+
+        /// <summary>
+        /// Варинаты ответа
+        /// </summary>
+        public int[] Variants { get; set; }
+
+
+        /// <summary>
+        /// Создать задачу
+        /// </summary>
+        public abstract string CreateProblem();
+    }
+}
